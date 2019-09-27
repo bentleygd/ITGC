7c7bbb814299fcdf12cc73e05a9d8cd0225fc871
Gabriel Bentley <gbentley@24hourfit.com>
2019-09-27 15:28:09 -0700

    modified:   itgcbin.py

--- itgcbin.py
+++ itgcbin.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from subprocess import run, PIPE
 from re import match, search
+from socket import gethostbyname, socket, AF_INET, SOCK_STREAM
+from socket import gaierror, timeout
 
 
 def getUsers(host):
@@ -27,8 +29,8 @@ def getGroups(host):
     # Obtaining groups (and members) that will be monitored.
     host_groups = run(
         ['/usr/bin/ssh', host, 'cat', '/etc/group'],
-         encoding='ascii', stdout=PIPE
-         ).stdout.strip('\n').split('\n')
+        encoding='ascii', stdout=PIPE
+        ).stdout.strip('\n').split('\n')
     for line in m_groups:
         groups.append(line.strip('\n'))
     print(groups)
@@ -39,3 +41,35 @@ def getGroups(host):
             if search(r_exp, host_group):
                 monitored_groups.append(host_group)
     return monitored_groups
+
+
+def getHosts(ossec_server):
+    """Returns a list of all servers connected to an OSSEC server."""
+    audited_hosts = {'active_hosts':[],'dead_hosts': []}
+    hosts = run(
+        ['/usr/bin/ssh', ossec_server, 'sudo', '/var/ossec/bin/agent_control',
+        '-ls'], encoding='ascii', stdout=PIPE).stdout.split('\n')
+    hostnames = []
+    for host in hosts:
+        if len(host) > 0:
+            hostnames.append(host.split(',')[1])
+    print(hostnames)
+    for hostname in hostnames[1:5]:
+        print('Connecting to ' +  hostname)
+        try:
+            host_ip = gethostbyname(hostname)
+            s = socket(AF_INET, SOCK_STREAM)
+            s.settimeout(2)
+            s.connect((host_ip, 22))
+            s.send(b'\n\n')
+            data = s.recv(4096)
+        except gaierror:
+            audited_hosts['dead_hosts'].append(hostname)
+            continue
+        except timeout:
+            audited_hosts['dead_hosts'].append(hostname)
+            continue
+        if not data is None and len(str(data)) > 0:
+            audited_hosts['active_hosts'].append(hostname)
+        s.close()
+    return audited_hosts
