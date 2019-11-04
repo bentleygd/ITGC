1cb8d984f1d2201bdb09b67c6ef5b739ce018ca6
bentleygd <gbentley@24hourfit.com>
2019-11-04 14:33:39 -0800

    Edited OSError exception handling.

--- itgcbin.py
+++ itgcbin.py
@@ -97,9 +97,8 @@ def get_linux_hosts(user, ossec_server):
         except timeout:
             audited_hosts['dead_hosts'].append(hostname)
             continue
-        except OSError as RouteError:
-            if RouteError[0] == 113:
-                audited_hosts['dead_hosts'].append(hostname)
+        except OSError:
+            audited_hosts['dead_hosts'].append(hostname)
             continue
         except ConnectionRefusedError:
             audited_hosts['dead_hosts'].append(hostname)
